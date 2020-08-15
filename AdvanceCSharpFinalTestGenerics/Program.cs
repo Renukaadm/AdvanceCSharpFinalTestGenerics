9c4b43c3cd2d8e6165eab03e2d29a6eeb48f7468
Renuka <renuka.aedma@gmail.com>
2020-08-15 10:23:20 -0400

    Add project files.

---
+++ AdvanceCSharpFinalTestGenerics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdvanceCSharpFinalTestGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class GenericClass<T> : IEnumerable<T>
+    {
+        private List<T> list { get; set; }
+
+        public void addElements(T item)
+        {
+            list.Add(item);
+        }
+
+        public void getElements(T item)
+        {
+            return item[0];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        public void sortElements(T item)
+        {
+            list.Sort();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
